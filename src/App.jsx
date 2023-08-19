ee98a6fdc463b3613bb478c87c8eafccfef4f708
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-08-19 15:25:18 +0800

    init w/ config issue:incompatible python deps for node-sass lib

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom";
 import AssemblyLine from "./components/AssemblyLine";
 
 /* Changes made to this file will not affect your tests.
- * This file is used to control the behavior of the web preview. 
-*/
-const App = props => (
+ * This file is used to control the behavior of the web preview.
+ */
+const App = (props) => (
   <div id="app">
-    <AssemblyLine 
-      stages={[
-        "Brainstorming", 
-        "Development", 
-        "Testing", 
-        "Deployment"
-      ]}
+    <AssemblyLine
+      stages={["Brainstorming", "Development", "Testing", "Deployment"]}
     />
+    <div>Hello world</div>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.scss";
+// import "./styles.scss";
 import App from "./App";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
